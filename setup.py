7d6ac5517da186253b01630a8dee094486f107f5
trichter <tom.eulenfeld@uni-jena.de>
2023-03-07 16:32:19 +0100

    remove py3.5 from ci matrix, update classifiers in setup.py

--- setup.py
+++ setup.py
@@ -35,10 +35,6 @@ CLASSIFIERS = [
     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
     'Topic :: Scientific/Engineering :: Physics'
     ]
 
